69e48538b084c353b5c038e3b91b4afdc2df1075
jokerbobo33 <chenjia9393@163.com>
2020-01-13 13:52:12 +0800

    第二次提交

--- data-server/routes/chatlist.js
+++ data-server/routes/chatlist.js
@@ -7,6 +7,7 @@ mongoose.connect('mongodb://27017/express', { useNewUrlParser: true });
 const Mock = require('mockjs');
 //子文档
 let pinglun = new mongoose.Schema({
+<<<<<<< HEAD
 item_id: Number,
 userhead: String,
 hot_name: String,
@@ -33,3 +34,43 @@ for (let i = 0; i < 10; i++) { //设置子文档 var pllist=[]; for (let j=0; j 
   , 'png' , '219x172' ) ], ori_con: "" , ori_name: "" } pllist.push(pl); } //将子文档添加到父文档 // chatlist.insertMany({ id: i,
   pinglun: pllist }); } // chatlist.insertMany(pllist); // chatlist.deleteMany({}).then(()=> callback())
   module.exports = chatlist;
+=======
+  item_id: Number,
+  userhead: String,
+  hot_name: String,
+  hot_date: Date,
+  agr_num: Number,
+  rec_num: Number,
+  hot_com: String,
+  picarrs: Array,
+  ori_con: String,
+  ori_name: String
+})
+//父文档
+let chatlist = mongoose.model('chatlist', new mongoose.Schema({
+  id: Number,
+  pinglun: [pinglun]
+}))
+//var pllist = [];
+
+for (let i = 0; i < 10; i++) {
+  //设置子文档 
+  var pllist = [];
+  for (let j = 0; j < 10; j++) {
+    let pl = {
+      item_id: j, userhead:
+        Mock.Random.image('65x65', '#00cc00', '#FFF', 'png', '65x65'), hot_name: Mock.Random.cname(), hot_date:
+        Mock.Random.date('yyyy-MM-dd'), agr_num: parseInt(Math.random() * 600), rec_num: parseInt(Math.random() * 600),
+      hot_com: Mock.Random.cparagraph(), picarrs: [Mock.Random.image('219x172', '#ccc000', '#FFF', 'png', '219x172'),
+      Mock.Random.image('219x172', '#ccc000', '#FFF', 'png', '219x172'), Mock.Random.image('219x172', '#ccc000', '#FFF'
+        , 'png', '219x172')], ori_con: "", ori_name: ""
+    }
+    pllist.push(pl);
+  }
+  //将子文档添加到父文档 
+  // chatlist.insertMany({ id: i,pinglun: pllist }); 
+}
+// chatlist.insertMany(pllist); 
+// chatlist.deleteMany({}).then(()=> callback())
+module.exports = chatlist;
+>>>>>>> 第二次提交
